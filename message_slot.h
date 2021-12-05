928df97465baeaedfa0bc4a488126f52934d3c7d
kerenpeer <kerenpeer97@gmail.com>
2021-12-05 12:48:02 +0200

    Update message_slot.h

--- message_slot.h
+++ message_slot.h
@@ -32,7 +32,8 @@ typedef struct msg_slot{
   struct channel *channels;
 } Msg_slot;
 
-struct Msg_slot driver[256] = {NULL};
+struct Msg_slot driver[256];
+memset(driver,0,256*sizeof(Msg_slot));
 
 static int device_open(struct inode* inode, struct file* file);
 static int device_release(struct inode* inode, struct file*  file);
